a3a67380a5ecc971657502cf9ea283ef01fad965
Jorge Ramos <jorginhodeveloper@gmail.com>
2021-08-20 19:50:39 -0300

    feat: adjust initial layout

--- src/components/cam-recorder/cam-recorder.tsx
+++ src/components/cam-recorder/cam-recorder.tsx
@@ -6,80 +6,49 @@ import { h, Component, State } from '@stencil/core';
   shadow: true,
 })
 export class CamRecorder {
-  @State() recordButton: object;
-  @State() rotateButton: object;
-  @State() flashButton: object;
-  @State() downloadButton: object;
-  @State() contentWrap: object;
-  @State() gumVideo: object;
-  @State() footer: object;
-  @State() capabilities: object;
-  @State() counter: object;
-  @State() safeZone: object;
-  @State() containerProgressBar: object;
-  @State() progressBar: object;
-  @State() videoCurrentTimeDisplay: object;
-  @State() frameSteps: object;
-  @State() timers: object;
-  @State() faceCam: boolean;
-  @State() torch: boolean;
-  @State() srcTimer: number;
-  @State() timer: number;
-  @State() videoTime: number;
-  @State() mediaRecorder: object;
-  @State() recordedBlobs: object;
-  @State() isMobile: boolean;
-  @State() stream: object;
-  @State() frameStepsIndex: number;
-  @State() frameStepsTimes: object;
-  @State() frameStepPhrases: object;
-  @State() maxWidth: number;
-  @State() maxHeight: number;
-  @State() aspectRatio: object;
+  @State() constraints: object;
+  @State() showCamera: boolean = false;
 
-  // connectedCallback() {
-  //   console.log('iniciouuuu');
-  //   this.gumVideo = shadowRoot.querySelector("video#gum")
-  // }
-
-  async showCamera() {
-    await this.initCamera({
+  connectedCallback() {
+    console.log('iniciouuuu');
+    this.showCamera = true;
+    this.constraints = {
       audio: false,
       video: {
-        facingMode: this.faceCam ? 'user' : 'environment',
-        advanced: [{ torch: this.torch }],
-        width: this.isMobile ? this.maxWidth : this.maxHeight,
-        height: this.isMobile ? this.maxHeight : this.maxWidth,
+        facingMode: 'user',
+        advanced: [{ torch: true }],
+        // width: this.isMobile ? this.maxWidth : this.maxHeight,
+        // height: this.isMobile ? this.maxHeight : this.maxWidth,
       },
-    });
+    };
+    this.initCamera(this.constraints);
   }
 
-  async initCamera(constraints) {
+  async initCamera(constraints: object) {
     try {
-      // console.log((await navigator.mediaDevices.enumerateDevices()).filter(item => item.kind === 'videoinput'))
+      // console.log((await navigator.mediaDevices.enumerateDevices()).filter(item => item.kind === 'videoinput'));
       const stream = await navigator.mediaDevices.getUserMedia(constraints);
-      this.handleSuccess(stream);
-      console.log('streammm');
-      console.log(this.stream);
-    } catch (e) {
-      console.error('navigator.getUserMedia error:', e);
+      // this.handleSuccess(stream);
+      // console.log('streammm');
+      console.log(stream);
+    } catch (error) {
+      console.error('navigator.getUserMedia error:', error);
     }
-  }
-
-  async handleSuccess(stream) {
-    // window.stream = stream;
-    this.stream = stream;
-    // this.gumVideo.srcObject = stream;
   }
 
   render() {
     return (
-      <div class="wrapper">
-        <section class="content-wrap">
-          <div id="safe-zone" class="safe-zone"></div>
+      // <div class="wrapper">
+      //   <section class="content-wrap">
+      //     <div id="safe-zone" class="safe-zone"></div>
+      //     <video id="gum" playsinline autoplay muted></video>
+      //   </section>
+      // </div>
+      this.showCamera && (
+        <div class="container">
           <video id="gum" playsinline autoplay muted></video>
-        </section>
-      </div>
+        </div>
+      )
     );
   }
 }
