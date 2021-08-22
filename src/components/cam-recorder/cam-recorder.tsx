8b145cba9152bea15b979beb502938f9b6c8cc75
Jorge Bierhals Ramos <jorginhodeveloper@gmail.com>
2021-08-22 15:17:41 -0300

    feat: add change cam

--- src/components/cam-recorder/cam-recorder.tsx
+++ src/components/cam-recorder/cam-recorder.tsx
@@ -17,21 +17,46 @@ export class CamRecorder {
   connectedCallback() {
     console.log('iniciouuuu');
     this.showCamera = true;
+    this.getConstraints();
+    // this.constraints = {
+    //   audio: false,
+    //   video: {
+    //     facingMode: this.frontalCamera ? "user" : "environment",
+    //     advanced: [{ torch: true }],
+    //     // width: this.isMobile ? this.maxWidth : this.maxHeight,
+    //     // height: this.isMobile ? this.maxHeight : this.maxWidth,
+    //   },
+    // };
+    // this.initCamera(this.constraints);
+    this.initCamera();
+  }
+
+  // componentShouldUpdate() {
+    // console.log('atualizouuuu');
+    // this.constraints = {
+    //   audio: false,
+    //   video: {
+    //     facingMode: this.frontalCamera ? "user" : "environment",
+    //     advanced: [{ torch: true }],
+    //   },
+    // };
+
+    // this.initCamera();
+  // }
+
+  getConstraints() {
     this.constraints = {
       audio: false,
       video: {
         facingMode: this.frontalCamera ? "user" : "environment",
         advanced: [{ torch: true }],
-        // width: this.isMobile ? this.maxWidth : this.maxHeight,
-        // height: this.isMobile ? this.maxHeight : this.maxWidth,
       },
     };
-    this.initCamera(this.constraints);
   }
 
-  async initCamera(constraints: object) {
+  async initCamera() {
     try {
-      const stream = await navigator.mediaDevices.getUserMedia(constraints);
+      const stream = await navigator.mediaDevices.getUserMedia(this.constraints);
       this.embedVideo = this.el.shadowRoot.querySelector('#gum');
       this.embedVideo.srcObject = stream;
     } catch (error) {
@@ -42,7 +67,8 @@ export class CamRecorder {
   changeCam() {
     console.log("virar cam");
     this.frontalCamera = !this.frontalCamera;
-    this.initCamera(this.constraints);
+    this.getConstraints();
+    this.initCamera();
   }
 
   render() {
